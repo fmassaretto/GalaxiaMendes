5aec9c7533e5d926eca26c4ac41129cc7457ce8a
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-13 01:42:37 -0300

    Added 3 new domains

---
+++ Galaxia.Dominio/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxia.Dominio
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public string Documento { get; set; }
+        public string Cor { get; set; }
+        public int QtdBracos { get; set; }
+        public int QtdPernas { get; set; }
+        public int QtdCabecas { get; set; }
+        public bool Respira { get; set; }
+    }
+}
---
+++ Galaxia.Dominio/Viagem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxia.Dominio
+{
+    [Table("Viagem")]
+    public class Viagem
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PlanetaOrigem { get; set; }
+        public string PlanetaDestino { get; set; }
+        public double Valor { get; set; }
+        public int Tempo { get; set; }
+    }
+}
